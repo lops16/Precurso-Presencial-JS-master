a8a0a050622f16f03c6d637afcf1620bf3256e3e
lops16 <alexalguacil.b@gmail.com>
2023-10-04 16:51:12 +0200

    Opcional parte 8 y correción parte 10

--- parte10/funciones_ejercicio_03/script.js
+++ parte10/funciones_ejercicio_03/script.js
@@ -1,5 +1,5 @@
 
-function divi(numA, numB){
+/* function divi(numA, numB){
     if (numB >= 1) {
         let resultado = numA/numB
         alert(resultado);
@@ -16,10 +16,34 @@ while (true) {
         break;
         
     }
-}
+} */
 
 
 /* let numero1 = parseInt(prompt("elige el primer numero"))
 let numero2 = parseInt(prompt("elige el segundo numero"))
 
 divi(numero1, numero2) */
+
+//solucion clase
+
+
+
+function divi(numOne, numTwo) {
+    if (numTwo !== 0) {
+        alert(numOne/numTwo)
+        
+    }
+}
+
+let firstNumber = ""
+let secondNumber = ""
+
+do{
+    firstNumber = parseInt("Dame un numero")
+    secondNumber = parseInt("dame otro numero")
+
+    divi (firstNumber, secondNumber)
+
+}while (secondNumber !== 0) {
+    
+}
